fa7cc9a928fdfdfb73e5075c977cf678b438840a
fabiorochaufsc <fabio.rocha@gmail.com>
2022-10-05 13:56:27 -0300

    exemplo cpp

---
+++ Unidade1/Codigos/exemplo_cpp/main/digital.cpp
@@ -0,0 +1,14 @@
+#include "digital.h"
+#include <stdio.h>
+
+int Digital::digitalRead(int pino)
+{
+    printf("Estou na digitalRead\n");
+    return 1;
+}
+
+void Digital::digitalWrite(int pino, int valor)
+{
+
+    
+}
---
+++ Unidade1/Codigos/exemplo_cpp/main/digital.h
@@ -0,0 +1,11 @@
+#ifndef __DIGITAL__
+#define __DIGITAL__
+
+class Digital {
+    public:
+        int  digitalRead (int pino);
+        void digitalWrite (int pino, int valor);
+
+};
+
+#endif
